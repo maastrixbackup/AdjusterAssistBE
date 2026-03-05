a332509a350f61bbd0df3accebc6ae282f4a61b4
Rudranarayan <rudranarayan.maastrix@gmail.com>
2026-03-05 14:44:44 +0530

    Authentication with JWT

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const authRoutes = require('./routes/auth.routes');
 
 const authRoutes = require('./routes/auth.routes');
 const userRoutes = require('./routes/user.routes')
