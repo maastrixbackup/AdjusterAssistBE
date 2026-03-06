f3f8d2f3bb2e9656b535103a7375808ad21372b9
Rudranarayan <rudranarayan.maastrix@gmail.com>
2026-03-06 11:19:56 +0530

    Feat: Settingup SQL server

--- src/app.js
+++ src/app.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { connectDB, sequelize } = require('./config/db'); // Import DB tools
+
 const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 
-// 1. Connect to Database
-connectDB();
 
-sequelize.sync({ force: false }) 
-    .then(() => console.log("Database models synced"))
-    .catch(err => console.log("Sync error:", err));
 
 app.use(cors());
 app.use(express.json());
@@ -23,8 +18,5 @@ app.get("/", (req, res) => {
 // 3. Routes
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
