b8b04a97cfb0c01cca3355bda9a5e5fae308dd0b
Rudranarayan <rudranarayan.maastrix@gmail.com>
2026-04-15 11:13:49 +0530

    Get File Metadata

--- src/routes/workspace.routes.js
+++ src/routes/workspace.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {createFile, getMyFiles, updateFile, deleteFile} = require("../controllers/file.controller");
+const {createFile, getMyFiles, updateFile, deleteFile, getFileById} = require("../controllers/file.controller");
 const {getFileDrafts} = require("../controllers/message.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
 
@@ -10,6 +10,7 @@ router.get("/", async(req, res)=>{
 
 router.post("/new", authMiddleware, createFile);
 router.get("/my-files", authMiddleware, getMyFiles);
+router.get("/:fileId", authMiddleware, getFileById);
 router.get("/:fileId/drafts", authMiddleware, getFileDrafts);
 router.put('/update/:fileId', authMiddleware, updateFile);
 router.delete("/delete/:fileId", authMiddleware, deleteFile)
